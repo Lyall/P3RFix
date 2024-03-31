65d810dcc1591fcb1c6386ad9318a5a08b1443fd
Lyall <lyall@stamper.net>
2024-03-31 04:52:51 +0100

    Version bump

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -12,7 +12,7 @@ HMODULE thisModule;
 inipp::Ini<char> ini;
 std::shared_ptr<spdlog::logger> logger;
 string sFixName = "P3RFix";
-string sFixVer = "1.1.7";
+string sFixVer = "1.1.8";
 string sLogFile = "P3RFix.log";
 string sConfigFile = "P3RFix.ini";
 string sExeName;
